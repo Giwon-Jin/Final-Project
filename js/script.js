98f8d69de25893e1b68fa387d96dbc21eb38be5b
Giwon-Jin <71603761+Giwon-Jin@users.noreply.github.com>
2020-12-21 01:21:10 +0900

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,327 @@
+$(document).ready(function(){
+
+	$('.yvd').click(function(){
+
+		$('.background_w').css({ 
+    	'display':'none'
+		});
+
+	    $('.title').css({ 
+    	'display':'none'
+		});
+
+		$('.sub_01').css({ 
+    	'display':'none'
+		});
+
+		$('.sub_02').css({ 
+    	'display':'none'
+		});
+
+		$('.sub_03').css({ 
+    	'display':'none'
+		});
+
+		$('.sub_04').css({ 
+    	'display':'none'
+		});
+
+		$('.sub_05').css({ 
+    	'display':'none'
+		});
+
+		$('.yvd').css({ 
+    	'display':'none'
+		});
+
+		$('.background_b').css({ 
+    	'display':'flex'
+		});
+
+		$('.title_B').css({ 
+    	'display':'inline-block'
+		});
+
+		$('.sub_01_B').css({ 
+    	'display':'inline-block'
+		});
+
+		$('.sub_02_B').css({ 
+    	'display':'inline-block'
+		});
+
+		$('.sub_03_B').css({ 
+    	'display':'inline-block'
+		});
+
+		$('.sub_04_B').css({ 
+    	'display':'inline-block'
+		});
+
+		$('.sub_05_B').css({ 
+    	'display':'inline-block'
+		});
+
+		$('.st0').css({ 
+    	'animation-play-state':'running'
+		});
+
+		$('.st1').css({ 
+    	'animation-play-state':'running'
+		});
+
+		$('.st2').css({ 
+    	'animation-play-state':'running'
+		});
+
+		$('.st3').css({ 
+    	'animation-play-state':'running'
+		});
+
+		$('.st4').css({ 
+    	'animation-play-state':'running'
+		});
+
+		$('.st5').css({ 
+    	'animation-play-state':'running'
+		});
+
+		$('.st6').css({ 
+    	'animation-play-state':'running'
+		});
+
+		$('.st7').css({ 
+    	'animation-play-state':'running'
+		});
+
+		$('.st8').css({ 
+    	'animation-play-state':'running'
+		});
+
+		$('.st9').css({ 
+    	'animation-play-state':'running'
+		});
+
+		$('.st10').css({ 
+    	'animation-play-state':'running'
+		});
+
+
+
+		
+
+
+
+	}); 
+
+	console.log($(".st0"))
+    var pathArray = $(".st0")
+
+
+
+   $.each(pathArray, function(i, d) {
+       console.log(i, d);
+
+
+       var pathLength = d.getTotalLength()
+       console.log(pathLength)
+
+       $(d).css("stroke-dasharray", pathLength + " " + pathLength)
+       $(d).css("stroke-dashoffset",pathLength)
+
+   });
+
+
+   	console.log($(".st1"))
+    var pathArray = $(".st1")
+
+
+
+   $.each(pathArray, function(i, d) {
+       console.log(i, d);
+
+
+       var pathLength = d.getTotalLength()
+       console.log(pathLength)
+
+       $(d).css("stroke-dasharray", pathLength + " " + pathLength)
+       $(d).css("stroke-dashoffset",pathLength)
+
+   });
+
+    console.log($(".st2"))
+    var pathArray = $(".st2")
+
+
+
+   $.each(pathArray, function(i, d) {
+       console.log(i, d);
+
+
+       var pathLength = d.getTotalLength()
+       console.log(pathLength)
+
+       $(d).css("stroke-dasharray", pathLength + " " + pathLength)
+       $(d).css("stroke-dashoffset",pathLength)
+
+   });
+
+
+    console.log($(".st3"))
+    var pathArray = $(".st3")
+
+
+
+   $.each(pathArray, function(i, d) {
+       console.log(i, d);
+
+
+       var pathLength = d.getTotalLength()
+       console.log(pathLength)
+
+       $(d).css("stroke-dasharray", pathLength + " " + pathLength)
+       $(d).css("stroke-dashoffset",pathLength)
+
+   });
+
+    console.log($(".st4"))
+    var pathArray = $(".st4")
+
+
+
+   $.each(pathArray, function(i, d) {
+       console.log(i, d);
+
+
+       var pathLength = d.getTotalLength()
+       console.log(pathLength)
+
+       $(d).css("stroke-dasharray", pathLength + " " + pathLength)
+       $(d).css("stroke-dashoffset",pathLength)
+
+   });
+
+       console.log($(".st5"))
+    var pathArray = $(".st5")
+
+
+
+   $.each(pathArray, function(i, d) {
+       console.log(i, d);
+
+
+       var pathLength = d.getTotalLength()
+       console.log(pathLength)
+
+       $(d).css("stroke-dasharray", pathLength + " " + pathLength)
+       $(d).css("stroke-dashoffset",pathLength)
+
+   });
+
+          console.log($(".st6"))
+    var pathArray = $(".st6")
+
+
+
+   $.each(pathArray, function(i, d) {
+       console.log(i, d);
+
+
+       var pathLength = d.getTotalLength()
+       console.log(pathLength)
+
+       $(d).css("stroke-dasharray", pathLength + " " + pathLength)
+       $(d).css("stroke-dashoffset",pathLength)
+
+   });
+
+             console.log($(".st7"))
+    var pathArray = $(".st7")
+
+
+
+   $.each(pathArray, function(i, d) {
+       console.log(i, d);
+
+
+       var pathLength = d.getTotalLength()
+       console.log(pathLength)
+
+       $(d).css("stroke-dasharray", pathLength + " " + pathLength)
+       $(d).css("stroke-dashoffset",pathLength)
+
+   });
+
+                console.log($(".st8"))
+    var pathArray = $(".st8")
+
+
+
+   $.each(pathArray, function(i, d) {
+       console.log(i, d);
+
+
+       var pathLength = d.getTotalLength()
+       console.log(pathLength)
+
+       $(d).css("stroke-dasharray", pathLength + " " + pathLength)
+       $(d).css("stroke-dashoffset",pathLength)
+
+   });
+
+
+             console.log($(".st9"))
+    var pathArray = $(".st9")
+
+
+
+   $.each(pathArray, function(i, d) {
+       console.log(i, d);
+
+
+       var pathLength = d.getTotalLength()
+       console.log(pathLength)
+
+       $(d).css("stroke-dasharray", pathLength + " " + pathLength)
+       $(d).css("stroke-dashoffset",pathLength)
+
+   });
+
+                console.log($(".st10"))
+    var pathArray = $(".st10")
+
+
+
+   $.each(pathArray, function(i, d) {
+       console.log(i, d);
+
+
+       var pathLength = d.getTotalLength()
+       console.log(pathLength)
+
+       $(d).css("stroke-dasharray", pathLength + " " + pathLength)
+       $(d).css("stroke-dashoffset",pathLength)
+
+   });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+})
+
+
+        
+
